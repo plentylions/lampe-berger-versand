f03a9e8971d9d8838e60d763e195afdc6936078f
Misha Yatsenko <1234asdfm@gmail.com>
2018-07-12 16:56:18 +0300

    menu link

--- src/Providers/lampeBergerVersandServiceProvider.php
+++ src/Providers/lampeBergerVersandServiceProvider.php
@@ -28,7 +28,20 @@ class lampeBergerVersandServiceProvider extends ServiceProvider
 
     public function boot(Twig $twig, Dispatcher $dispatcher, ConfigRepository $config)
     {
+        $dispatcher->listen('IO.init.templates', function (Partial $partial)
+        {
+            pluginApp(Container::class)->register('lampeBergerVersand::PageDesign.Partials.Header.NavigationList.twig', NavigationCacheSettings::class);
+            pluginApp(Container::class)->register('lampeBergerVersand::PageDesign.Partials.Header.SideNavigation.twig', SideNavigationCacheSettings::class);
 
+            $partial->set('head', 'Ceres::PageDesign.Partials.Head');
+            $partial->set('header', 'Ceres::PageDesign.Partials.Header.Header');
+            $partial->set('page-design', 'Ceres::PageDesign.PageDesign');
+            $partial->set('footer', 'Ceres::PageDesign.Partials.Footer');
+
+            $partial->set('header', 'lampeBergerVersand::PageDesign.Partials.Header.Header');
+
+            return false;
+        }, self::PRIORITY);
     }
 }
 
